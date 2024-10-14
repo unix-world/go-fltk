985b3ac7deec0ea3c4d80428872ada242e69d73a
Radu Ovidiu I. <ilies.ovidiu@googlemail.com>
2024-10-14 23:18:00 +0300

    HTML View Example

---
+++ examples/html/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	_ "embed"
+
+	"github.com/unix-world/go-fltk"
+)
+
+//go:embed example.htm
+var exampleText string
+
+const (
+	WIDGET_HEIGHT  = 600
+	WIDGET_PADDING = 10
+	WIDGET_WIDTH   = 400
+)
+
+func main() {
+	fltk.SetScheme("gtk+")
+
+	win := fltk.NewWindow(
+		WIDGET_WIDTH*2+WIDGET_PADDING*3,
+		WIDGET_HEIGHT*1+WIDGET_PADDING*2)
+	win.SetLabel("HTML Viewer")
+	win.Resizable(win)
+
+	hpack := fltk.NewPack(WIDGET_PADDING, WIDGET_PADDING, win.W(), WIDGET_HEIGHT)
+	hpack.SetType(fltk.HORIZONTAL)
+	hpack.SetSpacing(WIDGET_PADDING)
+
+	mdEditorBuf := fltk.NewTextBuffer()
+	mdEditorBuf.SetText(exampleText)
+	mdEditor := fltk.NewTextEditor(0, 0, WIDGET_WIDTH, WIDGET_HEIGHT)
+	mdEditor.SetBuffer(mdEditorBuf)
+
+	previewPanel := fltk.NewHelpView(0, 0, WIDGET_WIDTH, WIDGET_HEIGHT)
+	updatePreview(mdEditorBuf, previewPanel)
+
+	mdEditor.SetCallbackCondition(fltk.WhenChanged)
+	mdEditor.SetCallback(func() {
+		updatePreview(mdEditorBuf, previewPanel)
+	})
+
+	win.End()
+	win.Show()
+	fltk.Run()
+}
+
+func updatePreview(mdEditorBuf *fltk.TextBuffer, previewPanel *fltk.HelpView) {
+	source := []byte(mdEditorBuf.Text())
+	previewPanel.SetValue(string(source))
+}
+
